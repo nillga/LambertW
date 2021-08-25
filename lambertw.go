bf8ed7fdc68c17ae0b37cd65175fd57a45513bf8
nillga <71821765+nillga@users.noreply.github.com>
2021-08-25 22:47:50 +0200

    halleystep-iterator is created ahead

--- lambertw.go
+++ lambertw.go
@@ -87,60 +87,52 @@ func (w *w) router() (res float64) {
 	return res
 }
 func (w *w) router0() float64{
+	i := iterator{halleyStep,1}
 	if w.x < 1.38 {
 		if w.x < -0.311 {
 			if w.x < -0.367679 {
 				b := branch{order: 8, branch: 0}
 				return b.branchPointExpansion(w.x)
 			}
-			i := iterator{halleyStep,1}
 			b := branch{order: 10, branch: 0}
 			return i.recurse(w.x,b.branchPointExpansion(w.x))
 		}
-		i := iterator{halleyStep,1}
 		p := pade{branch: 0, N: 1}
 		return i.recurse(w.x,p.approximation(w.x))
 	}
-	if w.x < 236 { 
-		i := iterator{halleyStep,1}
+	if w.x < 236 {
 		p := pade{branch: 0, N: 2}
 		return i.recurse(w.x,p.approximation(w.x)) 
 	}
-	i := iterator{halleyStep,1}
 	b := branch{order: 6-1, branch: 0}
 	return i.recurse(w.x, b.asymptoticExpansion(w.x))
 }
 func (w *w) router1() float64 {
+	i := iterator{halleyStep,1}
 	if w.x < -0.0509 {
 		if w.x < -0.366079 {
 			if w.x < -0.367579 {
 				b := branch{order: 8, branch: -1}
 				return b.branchPointExpansion(w.x)
 			}
-			i := iterator{halleyStep, 1}
 			b := branch{order: 4, branch: -1}
 			return i.recurse(w.x, b.branchPointExpansion(w.x))
 		}
 		if w.x < -0.289379 {
-			i := iterator{halleyStep,1}
 			p := pade{branch: -1, N: 7}
 			return i.recurse(w.x, p.approximation(w.x))
 		}
-		i := iterator{halleyStep,1}
 		p := pade{branch: -1, N: 4}
 		return i.recurse(w.x, p.approximation(w.x))
 	}
 	if w.x < -0.000131826 {
-		i:= iterator{halleyStep,1}
 		p := pade{branch: -1, N: 5}
 		return i.recurse(w.x, p.approximation(w.x))
 	}
 	if w.x < -6.30957e-31 {
-		i:= iterator{halleyStep,1}
 		p := pade{branch: -1, N: 6}
 		return i.recurse(w.x, p.approximation(w.x))
 	}
-	i:= iterator{halleyStep,1}
 	b := branch{branch: -1, order: 3}
 	return i.recurse(w.x, b.logRecursion(w.x))
 }
