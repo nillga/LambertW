fb9aeea4c10c278bb589eec8ccbf12025ad9cd6b
nillga <71821765+nillga@users.noreply.github.com>
2021-08-25 21:16:38 +0200

    reformatting

--- lambertw.go
+++ lambertw.go
@@ -6,9 +6,9 @@ package lambertw
 *	 TODOS:
 *	 ------
 *
-*	- Implement Tests
+*	- Implement Tests -- Coverage: 49.0%
 *	- Benchmark this mess
-*	- Split in multiple files
+*	- Split in multiple files -- Routing file // methods
 *	- refactoring
 *	- add / test mezö integrals and / or fukushima methodz
 *
@@ -23,46 +23,42 @@ type w struct {
 	branch  int
 	x float64
 }
-
 type branch struct {
 	branch int
 	order int
 }
-
 type polynomial struct {
 	tag interface{}
 	order int
 }
-
 type iterator struct {
 	iterationStep iterationStep
 	depth int
 }
-
 type pade struct {
 	branch int
 	N int
 }
-
 type logRecursionImpl struct {
 	sgn float64
 	order int
 	branch int
 }
 
-type HORNERFUNC func(float64) float64
 type iterationStep func(float64,float64) float64
 
-var branchPoints = map[int]float64 {
-	0:-1, 1:1, 2:-0.333333333333333333e0, 3:0.152777777777777777e0, 4: -0.79629629629629630e-1,
-	5: 0.44502314814814814e-1, 6:-0.25984714873603760e-1, 7:0.15635632532333920e-1, 8:-0.96168920242994320e-2,
-	9:  0.60145432529561180e-2, 10: -0.38112980348919993e-2, 11:  0.24408779911439826e-2, 12: -0.15769303446867841e-2,
-	13: 0.10262633205076071e-2, 14: -0.67206163115613620e-3, 15:  0.44247306181462090e-3, 16: -0.29267722472962746e-3,
-	17:  0.19438727605453930e-3, 18: -0.12957426685274883e-3, 19:  0.86650358052081260e-4,
-}
-var asymptoticBs = [][]float64{
-	{0,-1},{0,1},{0,-1,0.5},{0,1,-3.0/2.0,1.0/3.0},{0,-1,3,-11.0/6.0,0.25},{0,1,-5,35.0/6.0,-25.0/12.0,0.2},
-}
+var (
+	branchPoints = map[int]float64 {
+		0:-1, 1:1, 2:-0.333333333333333333e0, 3:0.152777777777777777e0, 4: -0.79629629629629630e-1,
+		5: 0.44502314814814814e-1, 6:-0.25984714873603760e-1, 7:0.15635632532333920e-1, 8:-0.96168920242994320e-2,
+		9:  0.60145432529561180e-2, 10: -0.38112980348919993e-2, 11:  0.24408779911439826e-2, 12: -0.15769303446867841e-2,
+		13: 0.10262633205076071e-2, 14: -0.67206163115613620e-3, 15:  0.44247306181462090e-3, 16: -0.29267722472962746e-3,
+		17:  0.19438727605453930e-3, 18: -0.12957426685274883e-3, 19:  0.86650358052081260e-4,
+	}
+	asymptoticBs = [][]float64{
+		{0,-1},{0,1},{0,-1,0.5},{0,1,-3.0/2.0,1.0/3.0},{0,-1,3,-11.0/6.0,0.25},{0,1,-5,35.0/6.0,-25.0/12.0,0.2},
+	}
+)
 
 func W(branch int, x float64) float64 {
 	wOfX := new(w)
